08b6b3057cced7ecbb8d9a21dceeda33b195cd87
Alain <alain@gallib.net>
2023-03-30 13:54:49 +0200

    Test

--- nodejs-project/exo_test.js
+++ nodejs-project/exo_test.js
@@ -2,7 +2,7 @@ const utils = require('./utils.js');
 const assert = require('assert');
 const ex = require('./exo.js');
 
-it("test'", function () {
+it("création des variables'", function () {
     try
     {
         assert.strictEqual(5, ex.nombre1);
@@ -11,6 +11,20 @@ it("test'", function () {
         assert.strictEqual(12, ex.nombre2);
         assert.strictEqual(typeof(ex.nombre2), "number");
 
+        assert.strictEqual(17, ex.total);
+        assert.strictEqual(typeof(ex.total), "number");
+    }
+    catch (error)
+    {
+        utils.printMessage('Conseil 💡', "Vérifiez les noms et/ou les valeurs des variables 🤔");
+
+        throw error;
+    }
+});
+
+it("calcul du total'", function () {
+    try
+    {
         assert.strictEqual(17, ex.total);
         assert.strictEqual(typeof(ex.total), "number");
     }
