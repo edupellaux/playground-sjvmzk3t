d6fde8ac894015082f47a06b39850b5edfe57dd5
Alain <alain@gallib.net>
2023-03-30 12:53:11 +0200

    Test

--- nodejs-project/exo_test.js
+++ nodejs-project/exo_test.js
@@ -1,10 +1,10 @@
 let utils = require('./utils.js');
-let exo = require('./exo.js');
+const exo = require('./exo.js');
 let assert  = require('assert');
 
 describe('Test de constante', function() {
   it('La variable devrait être une constante', function() {
     const myConst = 42; // déclaration de la constante
-    assert.strictEqual(Object.getOwnPropertyDescriptor(globalThis, 'myConst').writable, false, 'La variable n\'est pas une constante'); //vérification de la constante
+    assert.strictEqual(Object.getOwnPropertyDescriptor(exo, 'LE_MODULE').writable, false, 'La variable n\'est pas une constante'); //vérification de la constante
   });
 });
