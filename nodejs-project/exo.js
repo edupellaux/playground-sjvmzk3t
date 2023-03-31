765e1b4de5bb1b8f0ba8dd92c71efe6f8a1ccc36
Alain <alain@gallib.net>
2023-03-31 20:24:39 +0200

    Test

--- nodejs-project/exo.js
+++ nodejs-project/exo.js
@@ -5,16 +5,14 @@ let grand = 15
 function test (petit, grand) {
 // }
 
-// Écrivez le code ici
 
-// Écrivez 3 variables : phrase
-if (petit < grand) {
-	phrase = "petit est plus petit"
-} else if (petit > grand) {
-	phrase = "petit est plus grand"
-} else phrase = "égalité"
+
+// Écrivez ci-dessous la condition
+
+
+
 // {
-	return {phrase}
+	return phrase
 }
 module.exports = {
 	test : test
