3b1da1d18ec918b9897b5d7c3395b66f8cf1f765
Alain <alain@gallib.net>
2023-04-01 06:24:03 +0200

    Test

--- nodejs-project/exo_test.js
+++ nodejs-project/exo_test.js
@@ -2,17 +2,19 @@ const utils = require('./utils.js');
 const assert = require('assert');
 const ex = require('./exo.js');
 
-it("création des variables'", function () {
-    try
-    {
-        assert.strictEqual("égalité", ex.test(5, 5));
-        assert.strictEqual("petit est plus petit", ex.test(5, 6));
-        assert.strictEqual("petit est plus grand", ex.test(6, 5));
-    }
-    catch (error)
-    {
-        utils.printMessage('Conseil 💡', "Vérifiez le nom et/ou la valeur de la variable 🤔");
+describe('Je teste describe' () => {
+	it("création des variables'", function () {
+	    try
+	    {
+	        assert.strictEqual("égalité", ex.test(5, 5));
+	        assert.strictEqual("petit est plus petit", ex.test(5, 6));
+	        assert.strictEqual("petit est plus grand", ex.test(6, 5));
+	    }
+	    catch (error)
+	    {
+	        utils.printMessage('Conseil 💡', "Vérifiez le nom et/ou la valeur de la variable 🤔");
 
-        throw error;
-    }
-});
+	        throw error;
+	    }
+	});
+})
--- statement.md
+++ statement.md
@@ -1,8 +1,3 @@
 # Test
 
 @[Test]({"stubs":["exo.js"], "command":"node_modules/mocha/bin/mocha exo_test.js --reporter list"})
-
-?[Soit la variable **let test="test"** Quel est le résultat de la concaténation ci-dessous ?]
--[ ] var
--[ ] let
--[x] const
